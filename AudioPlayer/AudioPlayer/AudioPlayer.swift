bdb03a84990cce9c0568f61e624df11837160893
Kevin Delannoy <delannoyk@gmail.com>
2015-09-19 21:08:39 +0200

    Changed shuffle method

--- AudioPlayer/AudioPlayer/AudioPlayer.swift
+++ AudioPlayer/AudioPlayer/AudioPlayer.swift
@@ -95,12 +95,9 @@ private extension NSObject {
 
 private extension Array {
     func shuffled() -> [Element] {
-        var list = self
-        for i in 0..<(list.count - 1) {
-            let j = Int(arc4random_uniform(UInt32(list.count - i))) + i
-            swap(&list[i], &list[j])
+        return sort { e1, e2 in
+            random() % 2 == 0
         }
-        return list
     }
 }
 
