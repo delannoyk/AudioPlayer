c1266705ac13a8b3416877156e0f05f272991137
Kevin Delannoy <delannoyk@gmail.com>
2016-03-09 18:10:12 +0100

    Added test files for event producers

---
+++ AudioPlayer/AudioPlayerTests/NetworkEventProducer_Tests.swift
@@ -0,0 +1,35 @@
+//
+//  NetworkEventProducer_Tests.swift
+//  AudioPlayer
+//
+//  Created by Kevin DELANNOY on 08/03/16.
+//  Copyright © 2016 Kevin Delannoy. All rights reserved.
+//
+
+import XCTest
+
+class NetworkEventProducer_Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
