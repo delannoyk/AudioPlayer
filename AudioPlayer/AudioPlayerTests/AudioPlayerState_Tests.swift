79c99c0a08e50bac521fc3d72a8f743161a0be8b
Kevin Delannoy <delannoyk@gmail.com>
2016-03-17 19:33:20 +0100

    Added tests for AudioPlayerState

---
+++ AudioPlayer/AudioPlayerTests/AudioPlayerState_Tests.swift
@@ -0,0 +1,55 @@
+//
+//  AudioPlayerState_Tests.swift
+//  AudioPlayer
+//
+//  Created by Kevin DELANNOY on 17/03/16.
+//  Copyright © 2016 Kevin Delannoy. All rights reserved.
+//
+
+import XCTest
+@testable import AudioPlayer
+
+class AudioPlayerState_Tests: XCTestCase {
+    func testEquatable() {
+        let buffering = AudioPlayerState.Buffering
+        let failedNoError = AudioPlayerState.Failed(nil)
+        let failedWithError = AudioPlayerState.Failed(NSError(domain: "", code: -1, userInfo: nil))
+        let paused = AudioPlayerState.Paused
+        let playing = AudioPlayerState.Playing
+        let stopped = AudioPlayerState.Stopped
+        let waitingForConnection = AudioPlayerState.WaitingForConnection
+
+        XCTAssertEqual(buffering, buffering)
+        XCTAssertEqual(failedNoError, failedNoError)
+        XCTAssertEqual(failedWithError, failedWithError)
+        XCTAssertEqual(paused, paused)
+        XCTAssertEqual(playing, playing)
+        XCTAssertEqual(stopped, stopped)
+        XCTAssertEqual(waitingForConnection, waitingForConnection)
+
+        XCTAssertNotEqual(buffering, failedNoError)
+        XCTAssertNotEqual(buffering, failedWithError)
+        XCTAssertNotEqual(buffering, paused)
+        XCTAssertNotEqual(buffering, playing)
+        XCTAssertNotEqual(buffering, stopped)
+        XCTAssertNotEqual(buffering, waitingForConnection)
+
+        XCTAssertNotEqual(failedNoError, failedWithError)
+        XCTAssertNotEqual(failedNoError, paused)
+        XCTAssertNotEqual(failedNoError, playing)
+        XCTAssertNotEqual(failedNoError, stopped)
+        XCTAssertNotEqual(failedNoError, waitingForConnection)
+
+        XCTAssertNotEqual(failedWithError, paused)
+        XCTAssertNotEqual(failedWithError, playing)
+        XCTAssertNotEqual(failedWithError, stopped)
+        XCTAssertNotEqual(failedWithError, waitingForConnection)
+
+        XCTAssertNotEqual(paused, playing)
+        XCTAssertNotEqual(paused, stopped)
+        XCTAssertNotEqual(paused, waitingForConnection)
+
+        XCTAssertNotEqual(playing, stopped)
+        XCTAssertNotEqual(playing, waitingForConnection)
+    }
+}
