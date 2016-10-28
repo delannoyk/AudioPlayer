78978612dce8ab429792294ce3de5d80071c70ee
Kevin Delannoy <delannoyk@gmail.com>
2016-10-27 21:47:21 -0400

    Added more tests on AudioPlayerState

--- AudioPlayer/AudioPlayerTests/AudioPlayerState_Tests.swift
+++ AudioPlayer/AudioPlayerTests/AudioPlayerState_Tests.swift
@@ -10,15 +10,15 @@ import XCTest
 @testable import AudioPlayer
 
 class AudioPlayerState_Tests: XCTestCase {
-    func testEquatable() {
-        let buffering = AudioPlayerState.buffering
-        let failedMaximumRetryCountHit = AudioPlayerState.failed(.maximumRetryCountHit)
-        let failedWithError = AudioPlayerState.failed(.foundationError(NSError(domain: "", code: -1, userInfo: nil)))
-        let paused = AudioPlayerState.paused
-        let playing = AudioPlayerState.playing
-        let stopped = AudioPlayerState.stopped
-        let waitingForConnection = AudioPlayerState.waitingForConnection
+    let buffering = AudioPlayerState.buffering
+    let failedMaximumRetryCountHit = AudioPlayerState.failed(.maximumRetryCountHit)
+    let failedWithError = AudioPlayerState.failed(.foundationError(NSError(domain: "", code: -1, userInfo: nil)))
+    let paused = AudioPlayerState.paused
+    let playing = AudioPlayerState.playing
+    let stopped = AudioPlayerState.stopped
+    let waitingForConnection = AudioPlayerState.waitingForConnection
 
+    func testEquatable() {
         XCTAssertEqual(buffering, buffering)
         XCTAssertEqual(failedMaximumRetryCountHit, failedMaximumRetryCountHit)
         XCTAssertEqual(failedWithError, failedWithError)
@@ -51,5 +51,56 @@ class AudioPlayerState_Tests: XCTestCase {
 
         XCTAssertNotEqual(playing, stopped)
         XCTAssertNotEqual(playing, waitingForConnection)
+    }
+
+    func testIsVariable() {
+        XCTAssert(buffering.isBuffering)
+        XCTAssertFalse(buffering.isFailed)
+        XCTAssertFalse(buffering.isPaused)
+        XCTAssertFalse(buffering.isPlaying)
+        XCTAssertFalse(buffering.isStopped)
+        XCTAssertFalse(buffering.isWaitingForConnection)
+
+        XCTAssertFalse(failedMaximumRetryCountHit.isBuffering)
+        XCTAssert(failedMaximumRetryCountHit.isFailed)
+        XCTAssertFalse(failedMaximumRetryCountHit.isPaused)
+        XCTAssertFalse(failedMaximumRetryCountHit.isPlaying)
+        XCTAssertFalse(failedMaximumRetryCountHit.isStopped)
+        XCTAssertFalse(failedMaximumRetryCountHit.isWaitingForConnection)
+
+        XCTAssertFalse(failedWithError.isBuffering)
+        XCTAssert(failedWithError.isFailed)
+        XCTAssertFalse(failedWithError.isPaused)
+        XCTAssertFalse(failedWithError.isPlaying)
+        XCTAssertFalse(failedWithError.isStopped)
+        XCTAssertFalse(failedWithError.isWaitingForConnection)
+
+        XCTAssertFalse(paused.isBuffering)
+        XCTAssertFalse(paused.isFailed)
+        XCTAssert(paused.isPaused)
+        XCTAssertFalse(paused.isPlaying)
+        XCTAssertFalse(paused.isStopped)
+        XCTAssertFalse(paused.isWaitingForConnection)
+
+        XCTAssertFalse(playing.isBuffering)
+        XCTAssertFalse(playing.isFailed)
+        XCTAssertFalse(playing.isPaused)
+        XCTAssert(playing.isPlaying)
+        XCTAssertFalse(playing.isStopped)
+        XCTAssertFalse(playing.isWaitingForConnection)
+
+        XCTAssertFalse(stopped.isBuffering)
+        XCTAssertFalse(stopped.isFailed)
+        XCTAssertFalse(stopped.isPaused)
+        XCTAssertFalse(stopped.isPlaying)
+        XCTAssert(stopped.isStopped)
+        XCTAssertFalse(stopped.isWaitingForConnection)
+
+        XCTAssertFalse(waitingForConnection.isBuffering)
+        XCTAssertFalse(waitingForConnection.isFailed)
+        XCTAssertFalse(waitingForConnection.isPaused)
+        XCTAssertFalse(waitingForConnection.isPlaying)
+        XCTAssertFalse(waitingForConnection.isStopped)
+        XCTAssert(waitingForConnection.isWaitingForConnection)
     }
 }
