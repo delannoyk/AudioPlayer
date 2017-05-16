b1107990ab87bde31a9469a30935aa607d24fbf8
Daniel Freiling <dfg@nota.dk>
2017-05-16 15:03:33 +0200

    fix: stop did not properly clean up current AVPlayerItem
    current AVPlayerItem could still be buffering when Audio Session was stopped, leading to a warning about running I/O.

--- AudioPlayer/AudioPlayer/player/extensions/AudioPlayer+Control.swift
+++ AudioPlayer/AudioPlayer/player/extensions/AudioPlayer+Control.swift
@@ -70,6 +70,7 @@ extension AudioPlayer {
 
         if let _ = player {
             player?.rate = 0
+            player?.replaceCurrentItem(with: nil)
             player = nil
         }
         if let _ = currentItem {
